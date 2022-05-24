4ebea8f8ee79d0b7e1aa84b126152c5e5c0c1949
Epoy <lenyajpatiarba@gmail.com>
2022-05-24 10:30:00 +0800

    commit

---
+++ database.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+
+
+def Database():
+    global conn, cursor 
+    
+    conn = sqlite3.connect('Database.db')
+    
+    conn.execute('''
+        CREATE TABLE student (stud_id INT PRIMARY KEY NOT NULL,
+                              stuno INT,
+                              studname TEXT,
+                              course TEXT,
+                              subject TEXT,
+                              prelim_grade INT,
+                              midterm_grade INT,
+                              final_grade INT,
+                              average_grade INT,
+                              remark TEXT,
+                              point_equivalent TEXT)
+                              
+                ''')
+    conn.commit()
+    conn.close()
+
+Database()
